f14659634aba07c530e4fd3c0e2ff2679a3aa000
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-03-27 13:55:33 +0530

    setup: expressTs backend is done to setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv = require("dotenv");
+import express from "express";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+   res.send("Hello World!");
+   return;
+});
+
+app.listen(8000, () => {
+   console.log("Server is running on port 8000");
+});
