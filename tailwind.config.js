7eec80e72653b9c81d6ee8633c8d73dea46a48be
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-03-30 06:32:46 +0530

    fix: taiwind.config.js fix becase tailwind not working properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ import { heroui } from "@heroui/react";
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
