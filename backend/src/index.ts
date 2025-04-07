9395c94a6c4466c5f35686b0e4b088f2bf182dee
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-04-07 23:26:29 +0530

    fix: cors added in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,16 +3,34 @@ import dotenv from "dotenv";
 import express, { Express } from "express";
 import { templateCreate } from "./controller/template";
 import { chat } from "./controller/chat";
-
-dotenv.config();
-const app: Express = express();
-app.use(express.json());
+import cors from "cors";
 
 export type ModelType =
   | "gemini-2.0-flash"
   | "gemini-2.0-flash-lite"
   | "gemini-2.0-flash-8b"
   | "gemini-1.5-flash";
+
+dotenv.config();
+const app: Express = express();
+app.use(express.json());
+
+const corpsOption = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Origin",
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Credentials",
+  ],
+};
+
+app.use(cors(corpsOption));
 
 export type MessageType = { role: string; content: string }[];
 
@@ -26,6 +44,7 @@ app.get("/", (req, res) => {
 app.post("/template", templateCreate);
 app.post("/chat", chat);
 
-app.listen(8000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("Server is running on port 8000");
 });
