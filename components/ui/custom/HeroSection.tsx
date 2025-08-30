bc4c410c4d2c678e8553d85d48e1ea215ddc022b
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-08-30 16:24:31 +0530

    feat: heading and body style added as well appliend in herosection

--- components/ui/custom/HeroSection.tsx
+++ components/ui/custom/HeroSection.tsx
@@ -4,14 +4,10 @@ function HeroSection() {
   return (
     <div className="h-screen w-full pt-14 bg-transparent">
       <div className="flex flex-col items-center pt-[180]">
-        <div className="text-center">
-          <h1 className="text-[43px] font-extrabold">
-            What do you want to build?
-          </h1>
-          <h3 className="text-gray-400">
-            Prompt, run, edit, and deploy full-stack{" "}
-            <span className="text-white">web</span> and{" "}
-            <span className="text-white">mobile</span> apps.
+        <div className="text-center flex flex-col gap-2">
+          <h1 className="heading1 font-bold">Ready to bring ideas to life?</h1>
+          <h3 className="body1 font-semibold text-richblue-300/50">
+            Build apps and websites effortlessly with just a conversation
           </h3>
         </div>
         <div className=" mt-8">
