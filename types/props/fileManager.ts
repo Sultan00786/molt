179df2b3dc7efdde9ef5b083df087cf6b36ce938
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-10-14 15:09:41 +0530

    feat: setSelectedFile type changes

--- types/props/fileManager.ts
+++ types/props/fileManager.ts
@@ -3,23 +3,23 @@ import { Dispatch, SetStateAction } from "react";
 
 export interface FileManagerProps {
   files: ChatItem[];
-  setSelectedFile: Dispatch<SetStateAction<FileItem>>;
+  setSelectedFile: Dispatch<SetStateAction<FileItem | null>>;
 }
 
 export interface FolderProps {
   file: FolderItem;
   spacing: number;
-  setSelectedFile: Dispatch<SetStateAction<FileItem>>;
+  setSelectedFile: Dispatch<SetStateAction<FileItem | null>>;
 }
 
 export interface RenderFileProps {
   files: ChatItem[];
   spacing: number;
-  setSelectedFile: Dispatch<SetStateAction<FileItem>>;
+  setSelectedFile: Dispatch<SetStateAction<FileItem | null>>;
 }
 
 export interface FileProps {
   file: FileItem;
   spacing: number;
-  setSelectedFile: Dispatch<SetStateAction<FileItem>>;
+  setSelectedFile: Dispatch<SetStateAction<FileItem | null>>;
 }
