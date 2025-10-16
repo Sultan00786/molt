436890d93aa40a6d0a965f440af359b3d80abb66
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-10-16 12:55:30 +0530

    feat: headers add in next config for Cross-Origin Isolatio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
